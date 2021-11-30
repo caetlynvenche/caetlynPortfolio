2512f36564c5694b785603f32cb664f5432441e7
Caetlyn Venche <caetlynvenche@gmail.com>
2021-11-30 00:34:48 -0700

    updates

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -3,9 +3,12 @@ import React from 'react'
 const ContactMe = () => {
     return (
         <div className="contact">
-            <h2>Contact Me</h2>
-            <h3>caetlynvenche@gmail.com</h3>
-            <h3>(208)713-8464</h3>
+            <h2>Contact Me:</h2>
+
+            <div>
+                <h3>caetlynvenche@gmail.com</h3>
+                <h3>(208)713-8464</h3>
+            </div>
         </div>
     )
 }
--- src/WhoAmI.js
+++ src/WhoAmI.js
@@ -3,9 +3,14 @@ import React from 'react'
 const WhoAmI = () => {
     return (
         <div className='whoami'>
-            <h2>Who Am I</h2>
-            <p>I have always adored making things. When I was younger, I focused mainly on crochet and drawing, but as I have gotten older, more of my focus has turned towards the technical side as well. I now make many of the same things as when I was younger, but also have been putting more time and focus into making websites and some games.</p> 
-            <p>I am excited to see what I am able to create in the future, and am grateful to be taking the steps into that future now.</p>
+            <div className='title'>
+                <h3>Hi, I'm</h3>
+                <h2>Caetlyn Venche</h2>
+            </div>
+            <div className='body'>
+                <p>I have always adored making things. When I was younger, I focused mainly on crochet and drawing, but as I have gotten older, more of my focus has turned towards the technical side as well. I now make many of the same things as when I was younger, but also have been putting more time and focus into making websites and some games.</p> 
+                <p>I am excited to see what I am able to create in the future, and am grateful to be taking the steps into that future now.</p>
+            </div>
         </div>
     )
 }
